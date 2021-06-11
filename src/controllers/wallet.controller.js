1de3a12456bac4401e930a5a12613f0bae86e0e7
Viktor Arinze Chinemelum <rxarinze@live.com>
2021-06-11 19:12:38 +0100

    new wallet creation

--- src/controllers/wallet.controller.js
+++ src/controllers/wallet.controller.js
@@ -1,6 +1,9 @@
+const Wallet = require("../models/wallet.model");
+const { ObjectID } = require('mongodb');
 class WalletController {
     static createWallet = async (req, res, next) => {
         try {
+            let user_id = ObjectID("60bd5f7dd2ddfd4dc011dca5");
             let body = req.body;
             let myWallet = new Wallet({
                 _id: {
@@ -12,10 +15,10 @@ class WalletController {
             myWallet.save(err => {
                 if (err)
                     return res.status(500).send({ err })
-
+                return res.status(200).send({ status: 200, message: `Your wallet #${body.name} has been created!` })
             });
         } catch (err) {
-
+            console.log(err)
         }
     }
 }
