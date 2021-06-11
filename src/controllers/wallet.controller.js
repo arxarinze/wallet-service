93340ee35e112813b1d1d7f261fab213bd395af7
Viktor Arinze Chinemelum <rxarinze@live.com>
2021-06-11 19:02:33 +0100

    refactored the wallet into an account and will now be managed accordingly

--- src/controllers/wallet.controller.js
+++ src/controllers/wallet.controller.js
@@ -1,7 +1,19 @@
 class WalletController {
     static createWallet = async (req, res, next) => {
         try {
-            console.log("entered")
+            let body = req.body;
+            let myWallet = new Wallet({
+                _id: {
+                    user_id,
+                    wallet: body.name
+                },
+                description: body.description
+            });
+            myWallet.save(err => {
+                if (err)
+                    return res.status(500).send({ err })
+
+            });
         } catch (err) {
 
         }
--- src/models/wallet.model.js
+++ src/models/wallet.model.js
@@ -7,6 +7,7 @@ const WalletSchema = new Schema({
         user_id: { type: String, required: true, id: true },
         wallet: { type: String, required: true }
     },
+    description: { type: String, required: true },
     createdAt: { type: Date, default: Date.now() },
     updatedAt: { type: Date, default: Date.now() }
 });
